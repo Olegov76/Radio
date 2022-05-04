8f3a8df78951adbe8e6cb21b4407c7be9ce6ac83
Oleg <69840541+Olegov76@users.noreply.github.com>
2022-05-04 18:29:54 +0500

    04.05

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,3 +1,4 @@
 console.log('components');
 import './components/select.js';
 import './components/tabs.js';
+import './components/validate.js';
---
+++ src/js/components/validate.js
@@ -0,0 +1,28 @@
+import { validateForms } from '../functions/validate-forms';
+const rules = [{
+        ruleSelector: '.input-name',
+        rules: [{
+            rule: 'required',
+            value: true,
+            errorMessage: 'Заполните имя!'
+        }]
+    },
+    {
+        ruleSelector: '.input-email',
+        rules: [{
+            rule: 'required',
+            value: true,
+            errorMessage: 'Заполните Email'
+        }],
+        rules: [{
+            rule: 'email',
+            value: true,
+            errorMessage: 'Введите корректный Email'
+        }]
+    },
+];
+const afterForm = () => {
+    console.log('Произошла отправка, тут можно писать любые действия');
+};
+
+validateForms('.abaut__form', rules, afterForm);
