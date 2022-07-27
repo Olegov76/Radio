9cb7c849d42779eeb092c1e3ff5f05640e639281
Olegov76 <katapga@list.ru>
2022-07-27 15:48:55 +0500

    27.07

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,11 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('.tabs-btn').forEach(function(tabBtn) {
-        tabBtn.addEventListener('click', function(event) {
-            const path = event.currentTarget.dataset.path
-            document.querySelectorAll('.content-none').forEach(function(tabsContent) {
-                tabsContent.classList.remove('content-activ')
-            })
-            document.querySelector(`[data-target="${path}"]`).classList.add('content-activ')
-        })
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".tabs-btn").forEach(function (tabBtn) {
+        tabBtn.addEventListener("click", function (event) {
+            const path = event.currentTarget.dataset.path;
+            document
+                .querySelectorAll(".content-none")
+                .forEach(function (tabsContent) {
+                    tabsContent.classList.remove("content-activ");
+                });
+            document
+                .querySelector(`[data-target="${path}"]`)
+                .classList.add("content-activ");
+        });
+    });
+});
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".tabs-btn2").forEach(function (tabBtn) {
+        tabBtn.addEventListener("click", function (event) {
+            const path = event.currentTarget.dataset.path;
+            document
+                .querySelectorAll(".content-none2")
+                .forEach(function (tabsContent) {
+                    tabsContent.classList.remove("content-activ2");
+                });
+            document
+                .querySelector(`[data-target="${path}"]`)
+                .classList.add("content-activ2");
+        });
+    });
+});
+// let tabBtn = document.querySelectorAll(".tabs-btn2");
+// console.log(tabBtn);
+// tabBtn.forEach(function (btn) {
+//     btn.classList.remove("playlists__tabs-btn--activ");
+// });
+// e.currentTarget.classList.edd("playlists__tabs-btn--activ");
